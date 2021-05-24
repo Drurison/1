5ccc10f398539de20aadaf0030d19c404f243093
Bradley Johnson <peekofwar5889@outlook.com>
2021-05-24 02:53:48 +0000

    Update reactor_control.lua

--- reactor_control.lua
+++ reactor_control.lua
@@ -1,3 +1,21 @@
---[[
-    There's nothing here yet. Sorry!
-]]
+
+crashScreen = function(...)
+    local pass, err = ...
+    if not pass and err then
+        if err == nil then err = '( no error given )' end
+        
+        term.setBackgroundColor(colors.black)
+        term.setTextColor(colors.white)
+        term.clear()
+        
+        term.setBackgroundColor(colors.blue)
+        paintutils.drawFilledBox(1,0,51,3)
+        term.setCursorPos(1,2)
+        cWrite('Something went wrong!')
+        
+        term.setBackgroundColor(colors.black)
+        printError('\n\n\n'..err..'\n')
+    end
+end
+
+crashScreen(false,"This program is still under development. If you wish to preview the program, please use the indev branch. \n\nDO NOT USE INDEV VERSIONS ON A REACTOR THAT YOU DO NOT WISH TO LOOSE; SERIOUS DAMAGES MAY OCCUR FROM FAULTY INDEV COMMITS.")
