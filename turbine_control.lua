b1a66afbbe1096ab66066ea2ef3deb491e0ecb8d
Bradley Johnson <peekofwar5889@outlook.com>
2021-06-20 03:18:51 +0000

    Update turbine_control.lua

--- turbine_control.lua
+++ turbine_control.lua
@@ -1,3 +1,26 @@
---[[
-    There's nothing here yet. Sorry!
-]]
+local function cWrite(text)
+    local w, h = term.getSize()
+	local cX,cY = term.getCursorPos()
+    term.setCursorPos(math.floor(w / 2 - text:len() / 2 + .5), cY)
+    io.write(text)
+end
+crashScreen = function(...)
+    local pass, err = ...
+    if not pass and err then
+        if err == nil then err = '( no error given )' end
+        
+        term.setBackgroundColor(colors.black)
+        term.setTextColor(colors.white)
+        term.clear()
+        
+        term.setBackgroundColor(colors.red)
+        paintutils.drawFilledBox(1,0,51,3)
+        term.setCursorPos(1,2)
+        cWrite('TURBINE CONTROL ERROR')
+        
+        term.setBackgroundColor(colors.black)
+        printError('\n\n\n'..err..'\n')
+    end
+end
+
+crashScreen(false,"Tubrine program does not yet exist.")
