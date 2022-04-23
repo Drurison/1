70bb074a9ed480fe338c52261064b7f4b1994e64
Bradley Johnson <peekofwar5889@outlook.com>
2022-04-23 20:51:34 +0000

    Update reactor_control.lua

--- reactor_control.lua
+++ reactor_control.lua
@@ -6,7 +6,7 @@ local program_info = {
 	version = {-- PUSHED TO MASTER
         string = '1.2.0a1',
 	    date = 'April 23, 2022',
-        build = 8,
+        build = 9,
     },
 	files = {
 		config = string.sub(shell.getRunningProgram(),1,#shell.getRunningProgram()-#shell.getRunningProgram():match("[^%.]*$")-1)..'.cfg',
@@ -203,9 +203,9 @@ args = {
             if string.lower(args.commandLine[i]) == "/verbose" then args.verbose = true end
             if string.lower(args.commandLine[i]) == "--verbose" then args.verbose = true end
             if string.lower(args.commandLine[i]) == "/update" then args.update = true
-                i=i+1 args.updateBranch = args.commandLine[i] or nil end
+                i=i+1 args.updateBranch = args.commandLine[i] and args.commandLine[i] end
             if string.lower(args.commandLine[i]) == "--update" then args.update = true
-                i=i+1 args.updateBranch = args.commandLine[i] or nil end
+                i=i+1 args.updateBranch = args.commandLine[i] and args.commandLine[i] end
             --dev.verbose(i, type(args.commandLine[i]), args.commandLine[i])
         end
         --sleep(1)
